294af9e583da64c56e052dcfa3c3e9e373056965
Demidog03 <olzhasktl362@gmail.com>
2026-03-16 21:02:18 +0500

    BOM and DOM

--- lesson12.js
+++ lesson12.js
@@ -50,12 +50,12 @@
 // console.log(h1.innerText)
 // h1.innerText = 'Goodbye'
 
-const h1Tags = document.querySelectorAll('h1')
-for (const h1 of h1Tags) {
-    console.log(h1)
-}
+// const h1Tags = document.querySelectorAll('h1')
+// for (const h1 of h1Tags) {
+//     console.log(h1)
+// }
 
-console.log(document.querySelector('h1#hello3'))
+// console.log(document.querySelector('h1#hello3'))
 
 
 // Задание 2
@@ -72,12 +72,157 @@ console.log(document.querySelector('h1#hello3'))
 
 // 2.2. Поменять имена в списке выше на числа от 0 по порядку (0, 1, 2 и т.д.)
 
-const liTags = document.querySelectorAll('#list li')
+// const liTags = document.querySelectorAll('#list li')
 
-liTags.forEach((li) => {
-    console.log(li.innerText)
-})
+// liTags.forEach((li) => {
+//     console.log(li.innerText)
+// })
 
-liTags.forEach((li, index) => {
-    li.innerText = index
-})
+// liTags.forEach((li, index) => {
+//     li.innerText = index
+// })
+
+
+// Добавление/Создание/Удаление DOM элемента
+
+// Удаление
+// const list = document.querySelector('#list')
+// list.remove()
+
+// Задание 3
+
+// Добавить к нескольким тегам на странице класс "forRemove". 
+// Далее написать JS код, который найдет в HTML все элементы с классом "forRemove" и удалит их.
+
+// const itemsForRemove = document.querySelectorAll('.forRemove')
+
+// for (const item of itemsForRemove) {
+//     item.remove()
+// }
+
+
+// Создание/Добавление элемента
+
+// const newH1 = document.createElement('h1')
+// newH1.innerText = 'Hello'
+// console.log(newH1)
+
+// document.body.appendChild(newH1)
+
+// newH1.style.color = 'red'
+
+
+// Задание 4
+
+// Создать с помощью js абзац (тег p). Добавить в него контент. 
+// Добавить к нему стили: размер 36px, жирный шрифт. Добавить абзац с текстом на страницу.
+
+// const newP = document.createElement('p')
+// document.body.appendChild(newP)
+// newP.innerText = 'Hello paragraph'
+// newP.style.fontSize = '36px'
+// newP.style.fontWeight = 'bold'
+
+
+
+// Задание 5
+
+// Написать функцию, которая принимает на вход 3 параметра: название тега, название цвета, содержимое. 
+// Функция должна сформировать необходимый тег, добавить необходимый стиль с цветом и внести содержимое.
+//  Вывести несколько таких сгенерированных тегов в консоль, затем отправить их на страницу.
+
+// function elementGenerator(tagName, tagColor, text) {
+//     const newElement = document.createElement(tagName)
+//     newElement.style.color = tagColor
+//     newElement.innerText = text
+//     return newElement
+// }
+
+// const newElement1 = elementGenerator('h3', 'pink', 'Hello world!')
+// console.log(newElement1)
+// document.body.appendChild(newElement1)
+
+// const newElement2 = elementGenerator('span', 'red', 'Hello span!')
+// console.log(newElement2)
+// document.body.appendChild(newElement2)
+
+
+// Задание 6
+// Вставить в страницу (в html документ) тег <select>.
+//  С помощью js (в цикле) добавить в этот select опции (option) под годы от 1960 по 2026.
+
+// const yearSelect = document.querySelector('#yearSelect')
+
+// for (let i = 1960; i <= 2026; i++) {
+//     const newOption = document.createElement('option')
+//     newOption.innerText = i
+//     yearSelect.append(newOption)
+// }
+
+
+// Задание 7
+
+// Вставить в страницу (в html документ) ul.
+// Предусмотреть в коде следующий массив:
+
+
+// const clients = [
+//     {name: "Женя", order: true},
+//     {name: "Кристина", order: true},
+//     {name: "Павел", order: false},
+//     {name: "Виолетта", order: false},
+//     {name: "Костя", order: true}
+// ]
+
+
+// Перебирать массив, для каждого элемента массива создать li, наполнить li текстом:
+// - Клиент Женя оплатил заказ
+// - Клиент Павел отменил заказ
+// ... остальные li с контентом
+
+
+// Маска получается такой: "Клиент ИМЯ СТАТУС заказ", 
+// где имя - свойство объекта (а объект здесь - это текущая ячейка массива),
+//  статус зависит от от свойства order: если true – то оплатил, если false – то отменил.
+
+// const ul = document.querySelector('#list')
+
+// const clients = [
+//     {name: "Женя", order: true},
+//     {name: "Кристина", order: true},
+//     {name: "Павел", order: false},
+//     {name: "Виолетта", order: false},
+//     {name: "Костя", order: true}
+// ]
+
+// clients.forEach((client) => {
+//     const li = document.createElement('li')
+
+//     // let status
+//     // if (client.order === true) {
+//     //     status = 'оплатил/а'
+//     // }
+//     // else {
+//     //     status = 'отменил/a'
+//     // }
+//     const status = client.order ? 'оплатил/а' : 'отменил/a'
+//     li.innerText = `Клиент ${client.name} ${status} заказ`
+
+//     ul.appendChild(li)
+// })
+
+
+
+// CLASS
+const h1 = document.querySelector('h1')
+// h1.className = 'red-text'
+// h1.className = ''
+
+// const div = document.querySelector('#container')
+// // div.className = 'red-text'
+// div.classList.add('red-text')
+// div.classList.remove('container')
+// console.log(div.classList.contains('red-text'))
+
+
+h1.classList.toggle('red-text')
