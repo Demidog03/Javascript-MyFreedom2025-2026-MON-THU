cf2e418363a67dc884250ae506b6052046ad1529
Demidog03 <olzhasktl362@gmail.com>
2026-04-26 21:02:43 +0500

    Принципы ООП

--- lesson18.js
+++ lesson18.js
@@ -181,64 +181,264 @@
 // - Круассан: 150тг
 // Итого к оплате: 450тг
 
-class ProductCard {
-    constructor(title, price, discount) {
-        this.title = title
-        this.price = price
-        this.discount = discount
-    }
+// class ProductCard {
+//     constructor(title, price, discount) {
+//         this.title = title
+//         this.price = price
+//         this.discount = discount
+//     }
 
-    getFinalPrice() {
-        return this.price - (this.price * this.discount/100)
+//     getFinalPrice() {
+//         return this.price - (this.price * this.discount/100)
+//     }
+
+//     showInfo() {
+//         console.log(`Товар: ${this.title}. Обычная цена: ${this.price}тг.` + (this.discount ? ` Цена по Ozon Карте (скидка ${this.discount}%): ${this.getFinalPrice()}тг!` : ''))
+//     }
+// }
+
+// const productCard1 = new ProductCard('Чехол на телефон', 2000, 15)
+// const productCard2 = new ProductCard('Робот-пылесос', 20_000, 7)
+// const productCard3 = new ProductCard('Смарт-часы', 30_000, 5)
+// const productCard4 = new ProductCard('Посудомойку', 800_000, 0)
+
+// productCard1.showInfo()
+// productCard2.showInfo()
+// productCard3.showInfo()
+// productCard4.showInfo()
+
+// class ShoppingCart {
+//     products = []
+
+//     addProduct(newProduct) {
+//         this.products.push(newProduct)
+//     }
+
+//     getTotalPrice() {
+//         let total = 0
+//         for (const product of this.products) {
+//             total += product.getFinalPrice()
+//         }
+//         return total
+//     }
+
+//     printReceipt() {
+//         console.log('Чек:')
+//         for (const product of this.products) {
+//             console.log(`- ${product.title}: ${product.getFinalPrice()}тг`)
+//         }
+//         console.log(`Итого: ${this.getTotalPrice()}тг`)
+//     }
+// }
+
+// const shoppingCart = new ShoppingCart()
+
+// shoppingCart.addProduct(productCard1)
+// shoppingCart.addProduct(productCard2)
+// shoppingCart.addProduct(productCard3)
+// shoppingCart.addProduct(productCard4)
+
+// console.log(shoppingCart)
+// console.log(shoppingCart.getTotalPrice())
+
+// shoppingCart.printReceipt()
+
+
+// Принципы ООП
+
+// 1. Инкапсуляция (Encapsulation)
+
+// class KaspiBank {
+//     #balance = 100_000; // приватное поле
+//     #pincode = null
+//     #attempts = 3
+
+//     constructor(pincode) {
+//         if (!pincode) {
+//             throw new Error('Пожалуйста введите пинкод!')
+//         }
+//         this.#pincode = pincode
+//     }
+
+//     #decreaseAttempts() {
+//         if (this.#attempts > 0) {
+//             this.#attempts--
+//         }
+//     }
+
+//     #checkAttempts() {
+//         if (this.#attempts <= 0) {
+//             console.error('Превышено кол-во попыток! Карта заблокирована')
+//             return false
+//         }
+//         return true
+//     }
+
+//     #resetAttempts() {
+//         this.#attempts = 3
+//     }
+
+//     deposit(amount, pincode) {
+//         if (!this.#checkAttempts()) {
+//             return
+//         }
+
+//         if (this.#pincode === pincode) {
+//             this.#balance += amount
+//             console.log(`Вам поступил платеж! Баланс: ${this.#balance}`)
+//             this.#resetAttempts()
+//         }
+//         else {
+//             this.#decreaseAttempts()
+//             console.error('Неверный пинкод!')
+//         }
+//     }
+
+//     changePincode(oldPincode, newPincode) {
+//         if (!this.#checkAttempts()) {
+//             return
+//         }
+
+//         if (oldPincode === this.#pincode) {
+//             this.#pincode = newPincode
+//             console.log('Пинкод успешно изменен!')
+//         }
+//         else {
+//             this.#decreaseAttempts()
+//             console.error('Неверно введен старый пинкод!')
+//         }
+//     }
+
+//     showBalance(pincode) {
+//         if (!this.#checkAttempts()) {
+//             return
+//         }
+
+//         if (this.#pincode === pincode) {
+//             console.log(`Текущий баланс: ${this.#balance}`)
+//         }
+//         else {
+//             this.#decreaseAttempts()
+//             console.error('Неверно введен старый пинкод!')
+//         }
+//     }
+// }
+
+// const myBank = new KaspiBank('1234')
+// // myBank.#balance = 1_000_000
+// // console.log(myBank.#balance)
+// console.log(myBank)
+// myBank.deposit(200_000, '1234')
+// myBank.deposit(50_000, '4321')
+// myBank.deposit(50_000, '4321')
+// myBank.deposit(200_000, '1234')
+// // myBank.changePincode('4321', '5555')
+// // myBank.changePincode('4321', '5555')
+// // myBank.changePincode('4321', '5555')
+// // myBank.changePincode('4321', '5555')
+
+// myBank.showBalance('1234')
+
+// Дополнить класс
+
+// Создать метод для смены пин кода - в методе требуется ввести старый пинкод и новый
+// Создать метод для проверки баланса
+// Доп: создать поле для отслеживания попытек неудачный смен пинкода
+
+
+// 2. Наследование (Inheritance)
+
+// class Product {
+//     constructor(name, price, weight, downloadLink) {
+//         this.name = name
+//         this.price = price
+//         this.weight = weight
+//         this.downloadLink = downloadLink
+//     }
+// }
+
+// class Product {
+//     constructor(name, price) {
+//         this.name = name
+//         this.price = price
+//     }
+
+//     showProductInfo() {
+//         console.log(`Название продукта: ${this.name}. Цена: ${this.price}`)
+//     }
+// }
+
+// class PhysicalProduct extends Product {
+//     constructor(name, price, weight) {
+//         super(name, price) // ссылаемся на constructor родительского класса
+//         this.weight = weight
+//     }
+// }
+
+// class DigitalProduct extends Product {
+//      constructor(name, price, downloadLink) {
+//         super(name, price) // ссылаемся на constructor родительского класса
+//         this.downloadLink = downloadLink
+//     }
+// }
+
+// const charger = new PhysicalProduct('Charger Xiaomi', 10_000, 2.3)
+// const subscriptionToSpotify = new DigitalProduct('Spotify Subscription', 2_500, 'https://download.spotify-subs.com')
+
+// console.log(charger)
+// console.log(subscriptionToSpotify)
+
+// charger.showProductInfo()
+// subscriptionToSpotify.showProductInfo()
+
+
+// Задание: IT-отдел
+
+// Создать базовый класс Employee -> name, baseSalary -> showInfo()
+// Создать класс Developer, который наследует Employee -> name, baseSalary, programmingLanguage
+// Создать класс Manager, который наследует Employee -> name, baseSalary, salaryBonus
+
+
+// 3. Полиморфизм (Polymorphism)
+class Employee {
+    constructor(name, baseSalary) {
+        this.name = name;
+        this.baseSalary = baseSalary;
     }
 
     showInfo() {
-        console.log(`Товар: ${this.title}. Обычная цена: ${this.price}тг.` + (this.discount ? ` Цена по Ozon Карте (скидка ${this.discount}%): ${this.getFinalPrice()}тг!` : ''))
+        return `Сотрудник: ${this.name}. Базовая зарплата: ${this.baseSalary}.`
     }
 }
 
-const productCard1 = new ProductCard('Чехол на телефон', 2000, 15)
-const productCard2 = new ProductCard('Робот-пылесос', 20_000, 7)
-const productCard3 = new ProductCard('Смарт-часы', 30_000, 5)
-const productCard4 = new ProductCard('Посудомойку', 800_000, 0)
-
-productCard1.showInfo()
-productCard2.showInfo()
-productCard3.showInfo()
-productCard4.showInfo()
-
-class ShoppingCart {
-    products = []
-
-    addProduct(newProduct) {
-        this.products.push(newProduct)
+class Developer extends Employee {
+    constructor(name, baseSalary, programmingLanguage) {
+        super(name, baseSalary);
+        this.programmingLanguage = programmingLanguage;
     }
 
-    getTotalPrice() {
-        let total = 0
-        for (const product of this.products) {
-            total += product.getFinalPrice()
-        }
-        return total
-    }
-
-    printReceipt() {
-        console.log('Чек:')
-        for (const product of this.products) {
-            console.log(`- ${product.title}: ${product.getFinalPrice()}тг`)
-        }
-        console.log(`Итого: ${this.getTotalPrice()}тг`)
+    showInfo() {
+        return super.showInfo() + " " + `Язык программирования: ${this.programmingLanguage}`
     }
 }
 
-const shoppingCart = new ShoppingCart()
+class Manager extends Employee {
+    constructor(name, baseSalary, salaryBonus) {
+        super(name, baseSalary);
+        this.salaryBonus = salaryBonus;
+    }
 
-shoppingCart.addProduct(productCard1)
-shoppingCart.addProduct(productCard2)
-shoppingCart.addProduct(productCard3)
-shoppingCart.addProduct(productCard4)
+    // Перезаписать метод showInfo() указав доп строку Зарплата с бонусами: salary + bonus
+    showInfo() {
+        return super.showInfo() + " " + `Зарплата с бонусами: ${this.baseSalary + this.salaryBonus}`
+    }
+}
 
-console.log(shoppingCart)
-console.log(shoppingCart.getTotalPrice())
+const dev = new Developer('Алексей', 500_000, 'JavaScript');
+const man = new Manager('Мария', 700_000, 100_000);
 
-shoppingCart.printReceipt()
+console.log(dev.showInfo());
+console.log(man.showInfo());
+
+console.log(dev);
+console.log(man);
